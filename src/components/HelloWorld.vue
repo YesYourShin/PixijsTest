f32c29c54b0ad227278a74cacee44ae6b79725cd
YesYourShin <ehrl827@naver.com>
2022-10-26 14:16:05 +0900

    안 되는 이유를 모르겠다

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,160 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <!-- <div id="change">
+      <router-link class="changepage" to="/OrdreCouches"> </router-link>
+    </div> -->
+    <div id="pixi"></div>
   </div>
 </template>
 
 <script>
+import * as PIXI from 'pixi.js';
+import dragonBones from 'pixi-dragonbones';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+
+  mounted() {
+    const app = new PIXI.Application({ antialias: true });
+    document.body.appendChild(app.view);
+    console.log(app.view);
+
+    app.stop();
+
+    const scale = 0.3;
+    const target = new PIXI.Point();
+    let armatureDisplay;
+
+    const animationNames = [
+      'PARAM_ANGLE_X',
+      'PARAM_ANGLE_Y',
+      'PARAM_ANGLE_Z',
+      'PARAM_EYE_BALL_X',
+      'PARAM_EYE_BALL_Y',
+      'PARAM_BODY_X',
+      'PARAM_BODY_Y',
+      'PARAM_BODY_Z',
+      'PARAM_BODY_ANGLE_X',
+      'PARAM_BODY_ANGLE_Y',
+      'PARAM_BODY_ANGLE_Z',
+      'PARAM_BREATH',
+    ];
+
+    // load spine data
+    PIXI.Loader.shared
+      .add('skeleton', 'examples/assets/pixi-dragonbones/eyetracking/shizuku_ske.json')
+      .add('texture_png_0', 'examples/assets/pixi-dragonbones/eyetracking/texture_00.png')
+      .add('texture_png_1', 'examples/assets/pixi-dragonbones/eyetracking/texture_01.png')
+      .add('texture_png_2', 'examples/assets/pixi-dragonbones/eyetracking/texture_02.png')
+      .add('texture_png_3', 'examples/assets/pixi-dragonbones/eyetracking/texture_03.png')
+      .load(onAssetsLoaded);
+
+    function onAssetsLoaded(loader, res) {
+      const factory = dragonBones.PixiFactory.factory;
+
+      factory.parseDragonBonesData(res.skeleton.data, 'shizuku');
+      factory.updateTextureAtlases([res.texture_png_0.texture, res.texture_png_1.texture, res.texture_png_2.texture, res.texture_png_3.texture], 'shizuku');
+      armatureDisplay = factory.buildArmatureDisplay('shizuku', 'shizuku');
+      armatureDisplay.animation.play('idle_00');
+      armatureDisplay.x = 400.0;
+      armatureDisplay.y = 500.0;
+      armatureDisplay.scale.set(scale, scale);
+      app.stage.addChild(armatureDisplay);
+
+      app.stage.interactive = true;
+      app.stage.on('touchmove', touchHandler);
+      app.stage.on('mousemove', touchHandler);
+
+      PIXI.Ticker.shared.add(enterFrameHandler);
+
+      app.start();
+    }
+
+    function touchHandler(event) {
+      const x = event.data.global.x;
+      const y = event.data.global.y;
+
+      target.x += ((x - app.stage.x - armatureDisplay.x) / 0.4 - target.x) * 0.3;
+      target.y += ((y - app.stage.y - armatureDisplay.y) / scale - target.y) * 0.3;
+    }
+
+    function enterFrameHandler() {
+      const armature = armatureDisplay.armature;
+      const animation = armatureDisplay.animation;
+      const canvas = armature.armatureData.canvas;
+
+      let p = 0.0;
+      const pX = Math.max(Math.min((target.x - canvas.x) / (canvas.width * 0.5), 1.0), -1.0);
+      const pY = -Math.max(Math.min((target.y - canvas.y) / (canvas.height * 0.5), 1.0), -1.0);
+      for (const animationName of animationNames) {
+        if (animation.hasAnimation(animationName)) {
+          let animationState = animation.getState(animationName, 1);
+          if (!animationState) {
+            animationState = animation.fadeIn(animationName, 0.1, 1, 1, animationName);
+            if (animationState) {
+              animationState.resetToPose = false;
+              animationState.stop();
+            }
+          }
+
+          if (animationState) {
+            switch (animationName) {
+              case 'PARAM_ANGLE_X':
+              case 'PARAM_EYE_BALL_X':
+                p = (pX + 1.0) * 0.5;
+                break;
+
+              case 'PARAM_ANGLE_Y':
+              case 'PARAM_EYE_BALL_Y':
+                p = (pY + 1.0) * 0.5;
+                break;
+
+              case 'PARAM_ANGLE_Z':
+                p = (-pX * pY + 1.0) * 0.5;
+                break;
+
+              case 'PARAM_BODY_X':
+              case 'PARAM_BODY_ANGLE_X':
+                p = (pX + 1.0) * 0.5;
+                break;
+
+              case 'PARAM_BODY_Y':
+              case 'PARAM_BODY_ANGLE_Y':
+                p = (-pX * pY + 1.0) * 0.5;
+                break;
+
+              case 'PARAM_BODY_Z':
+              case 'PARAM_BODY_ANGLE_Z':
+                p = (-pX * pY + 1.0) * 0.5;
+                break;
+
+              case 'PARAM_BREATH':
+                p = (Math.sin(armature.clock.time) + 1.0) * 0.5;
+                break;
+
+              default:
+                break;
+            }
+
+            animationState.currentTime = p * animationState.totalTime;
+          }
+        }
+      }
+    }
+  },
+
+  data() {
+    return {
+      app: new PIXI.Application({
+        width: 800,
+        height: 600,
+        backgroundColor: 0x1099bb,
+        resolution: window.devicePixelRatio || 1,
+      }),
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
